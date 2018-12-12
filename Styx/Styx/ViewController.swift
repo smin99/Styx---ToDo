ebd6fd415d5970e531abb02fb344d5f0ccd3926d
Seungmin <smhwang0715@gmail.com>
2018-12-12 09:38:43 -0800

    base settings

---
+++ Styx/Styx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Styx
+//
+//  Created by HwangSeungmin on 12/12/18.
+//  Copyright © 2018 Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
